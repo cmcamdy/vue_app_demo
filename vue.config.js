4682e06d73acd888ab6750ade7ec177ca3d4e2d1
ch98road <1027740945@qq.com>
2020-06-14 00:46:26 +0800

    0614

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,6 @@ module.exports = {
     // 指定子路径。比如将你的应用部署在
     // https://www.foobar.com/my-app/
     // 那么将这个值改为 '/my-app/'
-    baseUrl: "/", // 构建好的文件输出到哪里
 
     outputDir: "dist", // where to put static assets (js/css/img/font/...) // 是否在保存时使用‘eslint-loader’进行检查 // 有效值: true | false | 'error' // 当设置为‘error’时，检查出的错误会触发编译失败
     runtimeCompiler: false, // babel-loader默认会跳过`node_modules`依赖. // 通过这个选项可以显示转译一个依赖
@@ -37,7 +36,7 @@ module.exports = {
     pwa: {}, // configure webpack-dev-server behavior
     devServer: {
         open: process.platform === "darwin",
-        disableHostCheck: false,
+        disableHostCheck: true,
         host: "0.0.0.0",
         port: 8088,
         https: false,
